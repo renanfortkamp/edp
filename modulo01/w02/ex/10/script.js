78290f2e1122243277993427971fc4e58b1fc786
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2022-07-14 19:58:19 -0300

    ex10

---
+++ modulo01/w02/ex/10/script.js
@@ -0,0 +1,17 @@
+// Implemente um código copiando o array abaixo, e depois imprima no console o valor dos seguintes elementos:
+
+// var arr = [ 56, true, 'JS', 67, 99, 12, false, 88];
+
+// a) Imprima o valor do primeiro elemento;
+// b) Imprima o valor do último elemento;
+// c) Imprima o valor do terceiro elemento;
+// d) Imprima o tipo ("typeof") do valor do quinto elemento.
+
+
+
+var arr = [ 56, true, 'JS', 67, 99, 12, false, 88];
+
+console.log(arr[0])
+console.log(arr[arr.length-1])
+console.log(arr[2])
+console.log(typeof(arr[4]))
